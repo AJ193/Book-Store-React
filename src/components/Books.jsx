db02c80047ad853f7b0729b5daf9a78e6207bef9
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-06-07 02:03:32 +0500

    Update Books.js

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,21 +1,20 @@
-/* eslint-disable react/prop-types */
-
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Books.module.css';
 
-const Books = (props) => {
-  const { title, author } = props;
-  return (
-    <div>
-      <div className={StyleSheet.book}>
-        <div className={styles.description}>
-          <p>{title}</p>
-          <p>{author}</p>
-          <button type="button">Remove</button>
-        </div>
+const Book = ({ title, author }) => (
+  <div>
+    <div className={StyleSheet.book}>
+      <div className={styles.description}>
+        <p>{title}</p>
+        <p>{author}</p>
+        <button type="button">Remove</button>
       </div>
     </div>
-  );
+  </div>
+);
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
-
-export default Books;
+export default Book;
